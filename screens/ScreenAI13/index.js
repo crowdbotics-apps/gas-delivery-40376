bca99a1812373d1b064907a727082502a0dc2eff
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-05-10 00:26:17 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/ScreenAI13/index.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+const SubscriptionScreen = () => {
+  return <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Choose Your Plan</Text>
+        <Text style={styles.subtitle}>Upgrade to premium to unlock all features</Text>
+      </View>
+      <View style={styles.tiersContainer}>
+        <TouchableOpacity style={styles.tier}>
+          <Image source={require('../assets/bronze.png')} style={styles.tierImage} />
+          <Text style={styles.tierTitle}>Bronze</Text>
+          <Text style={styles.tierPrice}>$9.99/month</Text>
+          <Text style={styles.tierDescription}>Access to basic features</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tier}>
+          <Image source={require('../assets/silver.png')} style={styles.tierImage} />
+          <Text style={styles.tierTitle}>Silver</Text>
+          <Text style={styles.tierPrice}>$19.99/month</Text>
+          <Text style={styles.tierDescription}>Access to advanced features</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tier}>
+          <Image source={require('../assets/gold.png')} style={styles.tierImage} />
+          <Text style={styles.tierTitle}>Gold</Text>
+          <Text style={styles.tierPrice}>$29.99/month</Text>
+          <Text style={styles.tierDescription}>Access to all features</Text>
+        </TouchableOpacity>
+      </View>
+    </View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingTop: 50
+  },
+  header: {
+    marginBottom: 30
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666'
+  },
+  tiersContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  tier: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    borderRadius: 10,
+    padding: 20,
+    marginHorizontal: 10,
+    alignItems: 'center'
+  },
+  tierImage: {
+    width: 80,
+    height: 80,
+    marginBottom: 20
+  },
+  tierTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  tierPrice: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 10
+  },
+  tierDescription: {
+    fontSize: 14,
+    color: '#999'
+  }
+});
+export default SubscriptionScreen;
