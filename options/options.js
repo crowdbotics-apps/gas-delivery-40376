5fb8c5ad00bc415feba4ca7f0bfd18a105979999
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-05-10 00:02:23 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "little-sea-40376",
-  url: "https://little-sea-40376.botics.co",
-  api: "https://little-sea-40376.botics.co/api/v1"
-}
+  "name": "little-sea-40376",
+  "url": "https://little-sea-40376.botics.co",
+  "api": "https://little-sea-40376.botics.co/api/v1",
+  "initialRoute": "Splash"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
