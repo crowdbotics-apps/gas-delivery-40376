5b34258256d03b53cddfc582842b1a79d9c3497c
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-05-10 00:18:56 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/signup/index.js
+++ screens/signup/index.js
@@ -154,18 +154,20 @@ export default Signup;
 const Button = props => {
   const navigation = useNavigation();
   return <TouchableHighlight onPress={props.onPress} underlayColor='#DDDDDD'>
-      <View style={[btnStyles.button, {
-      backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
-      height: props.height ? props.height : 49,
-      borderWidth: props.borderWidth ? props.borderWidth : 0,
-      borderColor: props.borderColor ? props.borderColor : "#000000"
-    }]}>
-        <Text style={[btnStyles.text, {
-        color: props.color ? props.color : "#ffffff"
-      }]}>{props.children}</Text>
       <Pressable onPress={() => {
-        navigation.navigate("Privacy Policy");
-      }}><Text style={styles.NWzpOUQW}>{"Privacy policy"}</Text></Pressable></View>
+      navigation.navigate("profile");
+    }}><View style={[btnStyles.button, {
+        backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
+        height: props.height ? props.height : 49,
+        borderWidth: props.borderWidth ? props.borderWidth : 0,
+        borderColor: props.borderColor ? props.borderColor : "#000000"
+      }]}>
+        <Text style={[btnStyles.text, {
+          color: props.color ? props.color : "#ffffff"
+        }]}>{props.children}</Text>
+      <Pressable onPress={() => {
+          navigation.navigate("Privacy Policy");
+        }}><Text style={styles.NWzpOUQW}>{"Privacy policy"}</Text></Pressable></View></Pressable>
     </TouchableHighlight>;
 };
 
